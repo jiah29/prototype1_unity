1f6346e728caf056df88e275e6ffb64fffbadfc7
Jia Hao <71251813+jiah29@users.noreply.github.com>
2021-05-28 12:14:30 +0800

    Add files via upload
    A plane driving game

---
+++ Challenge I/PropellerSpinX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpinX : MonoBehaviour
+{
+    private float turnSpeed = 45.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.Rotate(Vector3.back * turnSpeed);
+    }
+}
