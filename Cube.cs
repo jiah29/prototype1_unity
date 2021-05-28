84afafd2b9f5ce7c32d6a2f5f0ec7c6d55597714
Jia Hao <71251813+jiah29@users.noreply.github.com>
2021-05-28 12:12:53 +0800

    Add files via upload
    Cube Animation

---
+++ Cube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 1.3f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(2.0f, 1.0f, 0.3f, 0.4f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(20.0f * Time.deltaTime, 2.0f, 0.0f);
+        transform.Translate(Vector3.back * Time.deltaTime * 20.0f);
+    }
+}
